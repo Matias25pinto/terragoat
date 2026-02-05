5398006cf5de2d6230e48e83e818dd45a962370f
Matias Pinto <matias.pinto@konecta.com.py>
2026-02-05 15:31:39 -0300

    Solucionar Vulnerabilidad AC_AWS_0214

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "data" {
   # bucket does not have access logs
   # bucket does not have versioning
   bucket        = "${local.resource_prefix.value}-data"
-  force_destroy = true
+  force_destroy = false
   tags = merge({
     Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
@@ -18,6 +18,16 @@ resource "aws_s3_bucket" "data" {
     git_repo             = "terragoat"
     yor_trace            = "0874007d-903a-4b4c-945f-c9c233e13243"
   })
+  versioning {
+    enabled = true
+  }
+  lifecycle_rule {
+    enabled = true
+
+    noncurrent_version_expiration {
+      days = 90
+    }
+  }
 }
 
 resource "aws_s3_bucket_object" "data_object" {
